aeab8b06b5fd1da15a45d17860b00097b396334b
Nick <sshedl22@gmail.com>
2021-03-14 13:14:43 +0300

    Done with binary search by value

--- BinarySearchByValue.py
+++ BinarySearchByValue.py
@@ -1,9 +1,50 @@
 import numpy as np
 import typing as ty
+import matplotlib.pyplot as plt
 
-def bs_by_value(array : np.ndarray, amount : int):
-    pass
 
+class BinarySearchByVal:
+
+    def __init__(self, array: np.ndarray, amount: int):
+        self.array = array
+        self.amount = amount
+
+    def perform(self) -> int:
+        left = -1
+        right: int = (self.array[-1] - self.array[0]) // self.amount
+        while right - left > 1:
+            mid: int = (right + left) // 2
+            if self._check(mid):
+                right = mid
+            else:
+                left = mid
+            # print("Debug: ", right, left)
+        return right
+
+    def _check(self, size: int) -> bool:
+        start = 0
+        closed_points = 0
+        arr_len = len(self.array)
+        for _ in range(self.amount):
+            closed_points += 1
+            for i in range(start + 1, arr_len):
+                if self.array[i] - self.array[start] > size:
+                    start = i
+                    break
+                closed_points += 1
+            # есть случаи когда отрезки настолько длинные что покрывают все точки за меньшее K,
+            # но без этого условия check вернет false из за строчек 27-28, поэтому нужно либо
+            # проверять что все точки покрыты после каждого прохода
+            # 1го цикла либо ставить условие closed_points >= arr_len
+            if closed_points == arr_len:
+                return True
+        return False
+
+    def __repr__(self):
+        return f"k - {self.amount}, array - {self.array}"
+
+    def __str__(self):
+        return f"k - {self.amount}, array - {self.array}"
 
 
 def parse_input(path_to_input: str) -> ty.Tuple[np.ndarray, int]:
@@ -11,14 +52,15 @@ def parse_input(path_to_input: str) -> ty.Tuple[np.ndarray, int]:
         input_ = r.readlines()
     input_ = list(map(str.strip, input_))
     k = int(input_[1])
-    print(k)
     source = np.array(input_[2:], dtype=np.int64)
-    print(source)
     return source, k
 
 
 if __name__ == '__main__':
     # start_sum()
-    source, to_find = parse_input("BSVTESTS/1.in")
+    source_arr, amount = parse_input("BSVTESTS/5.in")
+    # plt.plot([source_arr[i + 1] - source_arr[i] for i in range(len(source_arr) - 1)])
+    # plt.show()
+    print(BinarySearchByVal(source_arr, amount).perform())
     # for val in to_find:
     #     print(binary_search(source, val))
